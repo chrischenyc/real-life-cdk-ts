612056b189ddad869423ea310bdf7fa77680aa5d
Chris Chen <chris@capturedlabs.com>
2022-07-30 17:33:27 +1000

    test(rest-api-ecs): configure integration test

--- packages/rest-api-ecs/jest.config.js
+++ packages/rest-api-ecs/jest.config.js
@@ -1,6 +1,6 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
+/**
+ * Jest config for unit test
+ * @see https://jestjs.io/docs/configuration
  */
 
 module.exports = {
